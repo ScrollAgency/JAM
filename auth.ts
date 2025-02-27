679e7d6a6fbb7b2b4b1e91504bc261cf92c9453b
SashaCarton <163831379+SashaCarton@users.noreply.github.com>
2025-02-27 14:56:31 +0100

    Activation des importations et des variables d'environnement Supabase dans auth.ts

--- auth.ts
+++ auth.ts
@@ -1,19 +1,19 @@
-// import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl1 = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl1, supabaseAnonKey);
 
-// export const signInWithGoogle = async () => {
-//     const { error } = await supabase.auth.signInWithOAuth({
-//         provider: 'google',
-//         options: {
-//             redirectTo: window.location.origin, // Redirection après connexion
-//         },
-//     });
+export const signInWithGoogle = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+            redirectTo: window.location.origin, // Redirection après connexion
+        },
+    });
 
-//     if (error) {
-//         console.error('Erreur de connexion:', error.message);
-//     }
-// };
+    if (error) {
+        console.error('Erreur de connexion:', error.message);
+    }
+};
