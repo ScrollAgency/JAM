adf5b36e0130628a0a605ebe0300136130e9660d
Mariusdev <66720644+mariusdjen@users.noreply.github.com>
2025-07-10 09:19:41 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,9 @@ import React from "react";
 
 export default function Document() {
 	return (
-		<Html>
+		<Html lang="fr">
 			<Head>
 				{/* Script Weglot - Remplace TA_CLE_API_WEGL0T par ta clé API Weglot */}
-				
 				<script
 					type="text/javascript"
 					src="https://cdn.weglot.com/weglot.min.js"
@@ -15,7 +14,10 @@ export default function Document() {
 					dangerouslySetInnerHTML={{
 						__html: `
 							Weglot.initialize({
-								api_key: 'wg_7a994a95d8a52ee847d1d76f13c919c67'
+								api_key: 'wg_7a994a95d8a52ee847d1d76f13c919c67',
+								originalLanguage: 'fr',
+								destinationLanguages: ['en', 'es'],
+								autoSwitch: false
 							});
 						`,
 					}}
