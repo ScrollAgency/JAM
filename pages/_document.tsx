43cfb173b25232456bd266f3836ee2c362ec8932
Mariusdev <66720644+mariusdjen@users.noreply.github.com>
2025-07-10 10:05:05 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,15 +5,16 @@ export default function Document() {
 	return (
 		<Html lang="fr">
 			<Head>
-				{/* Script LocalizeJS avec configuration complète */}
+				{/* Script LocalizeJS avec la nouvelle clé et limitation aux langues français et anglais */}
 				<script src="https://global.localizecdn.com/localize.js"></script>
 				<script
 					dangerouslySetInnerHTML={{
 						__html: `
 							(function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","getSourceLanguage","detectLanguage","getAvailableLanguages","setWidgetLanguages","hideLanguagesInWidget","untranslatePage","bootstrap","prefetch","on","off","hideWidget","showWidget"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}})(window);
 							Localize.initialize({
-								key: 'svsjYf6tVXtrw',
+								key: 'lr6dZKfR5aBxR',
 								rememberLanguage: true,
+								languages: ['fr', 'en']
 							});
 						`,
 					}}
