94996b0be63416b99b7b4a1b3f1fd430f920d613
Gael <prigentgael@gmail.com>
2025-07-19 10:59:28 +0200

    Fix api in utils 7

--- pages/auth/callback.tsx
+++ pages/auth/callback.tsx
@@ -1,18 +1,27 @@
 // pages/auth/callback.tsx
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const code = context.query.code
+  const next = context.query.next ?? '/'
+
+  if (typeof code === 'string') {
+    return {
+      redirect: {
+        destination: `/api/supabase/callback?code=${code}&next=${encodeURIComponent(next as string)}`,
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    redirect: {
+      destination: '/auth/auth-code-error',
+      permanent: false,
+    },
+  }
+}
 
 export default function CallbackPage() {
-  const router = useRouter()
-
-  useEffect(() => {
-    const code = router.query.code
-    const next = router.query.next ?? '/'
-    if (typeof code === 'string') {
-      const encodedNext = encodeURIComponent(next as string)
-      router.replace(`/api/supabase/callback?code=${code}&next=${encodedNext}`)
-    }
-  }, [router])
-
   return <p>Connexion en cours...</p>
 }
