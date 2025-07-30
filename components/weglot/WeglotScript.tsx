3d0dec14638af6780cc6a14fb1399fb5f25380d0
Mariusdev <marius@agence-scroll.com>
2025-07-30 09:20:19 +0200

    Update WeglotScript.tsx

--- components/weglot/WeglotScript.tsx
+++ components/weglot/WeglotScript.tsx
@@ -4,28 +4,19 @@ import { useEffect } from 'react';
 
 export default function WeglotScript() {
   useEffect(() => {
-    const allowedPaths = [
-      '/',
-      '/login',
-      '/register',
-      '/register-candidat',
-      '/register-company',
-    ];
-    if (typeof window !== 'undefined' && allowedPaths.includes(window.location.pathname)) {
-      const script = document.createElement('script');
-      script.src = 'https://cdn.weglot.com/weglot.min.js';
-      script.async = true;
-      script.onload = () => {
-        // @ts-ignore
-        Weglot.initialize({
-          api_key: 'wg_7a994a95d8a52ee847d1d76f13c919c67',
-          originalLanguage: 'fr',
-          destinationLanguages: ['en', 'es'],
-          autoSwitch: false,
-        });
-      };
-      document.head.appendChild(script);
-    }
+    const script = document.createElement('script');
+    script.src = 'https://cdn.weglot.com/weglot.min.js';
+    script.async = true;
+    script.onload = () => {
+      // @ts-ignore
+      Weglot.initialize({
+        api_key: 'wg_7a994a95d8a52ee847d1d76f13c919c67',
+        originalLanguage: 'fr',
+        destinationLanguages: ['en', 'es'],
+        autoSwitch: false,
+      });
+    };
+    document.head.appendChild(script);
   }, []);
 
   return null;
